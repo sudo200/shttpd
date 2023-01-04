74548490bb5243715f8efee2dba2406f16840943
sudo200 <65343982+sudo200@users.noreply.github.com>
2023-01-04 17:31:46 +0100

    Add socket

--- src/main.c
+++ src/main.c
@@ -1,24 +1,29 @@
 #include "malloc_override.h"
 #include "server.h"
 
+#include <setjmp.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <setjmp.h>
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 #include <sutil/dmem.h>
 #include <sutil/logger.h>
 #include <sutil/util.h>
 
+#define die(msg)  do { logger_fatal_f(server.log, "%s: %m", msg); exit(EXIT_FAILURE); } while(0)
 
-static bool run = true;
-static int exit_code;
-static jmp_buf exit_buf;
+static volatile bool run = true;
+static volatile int exit_code;
+static jmp_buf jump;
 
 __attribute__((noreturn)) static void server_exit(int code) {
   run = false;
   exit_code = code;
-  longjmp(exit_buf, 0);
+  longjmp(jump, 1);
 }
 
 http_server_t server = {
@@ -30,6 +35,12 @@ __attribute__((constructor)) static void constructor(void) {
   ualloc = malloc;
   urealloc = realloc;
   ufree = free;
+
+  server.log = logger_new(NULL, NULL, false);
+}
+
+__attribute__((destructor)) static void destructor(void) {
+  logger_destroy(server.log);
 }
 
 
@@ -52,30 +63,74 @@ void sighandler(int sig) {
   }
 }
 
-int main(int argc, char **argv)
+__attribute__((noreturn)) int main(int argc, char **argv)
 {
-  server.log = logger_new(NULL, NULL, false);
+  fd_t srv_sock;
+  struct sockaddr addr = {
+    .sa_family = AF_INET
+  };
+  socklen_t addrlen;
 
-  logger_notice(server.log, "Starting shttpd...");
+  switch(addr.sa_family) {
+    case AF_INET: {
+        struct sockaddr_in *in_addr = (struct sockaddr_in *)&addr;
+        inet_aton("0.0.0.0", &in_addr->sin_addr);
+        in_addr->sin_port = htons(8080);
+        addrlen = sizeof(*in_addr);
+      }
+      break;
 
-  setsignal(SIGINT, sighandler);
-  setsignal(SIGTERM, sighandler);
-  setsignal(SIGHUP, sighandler);
-  setsignal(SIGSEGV, sighandler);
+    case AF_UNIX: {
+        struct sockaddr_un *un_addr = (struct sockaddr_un *)&addr;
+        strncpy(un_addr->sun_path, "socket.sock", 108);
+        unlink(un_addr->sun_path);
+        addrlen = sizeof(*un_addr);
+      }
+      break;
 
-  logger_notice(server.log, "shttp started!");
-
-  setjmp(exit_buf);
-  while (run) {
-    sleep(1);
+    default: {
+      logger_fatal(server.log, "Address family not supported!");
+      exit(EXIT_FAILURE);
+    }
   }
 
-  logger_notice(server.log, "Shutting down!");
+  { // Init
+    logger_notice(server.log, "Starting shttpd...");
 
-  sleep(2);
+    setsignal(SIGINT, sighandler);
+    setsignal(SIGTERM, sighandler);
+    setsignal(SIGHUP, sighandler);
+    setsignal(SIGSEGV, sighandler);
 
-  logger_info_f(server.log, "Exitting with code '%d'! Goodbye!\n", exit_code);
-  logger_destroy(server.log);
-  return exit_code;
+    if((srv_sock = socket(addr.sa_family, SOCK_STREAM, 0)) < 0)
+      die("socket");
+
+    const int tru = true;
+    if(setsockopt(srv_sock, SOL_SOCKET, SO_REUSEADDR, &tru, sizeof(tru)) < 0)
+      die("setsockopt");
+
+    if(bind(srv_sock, &addr, addrlen) < 0)
+      die("bind");
+
+    if(listen(srv_sock, 0xFF) < 0)
+      die("listen");
+
+    logger_notice(server.log, "shttp started!");
+  }
+  
+  setjmp(jump);
+  while (run) {
+  }
+
+  
+  { // Clean up
+    logger_notice(server.log, "Shutting down!");
+    shutdown(srv_sock, SHUT_RDWR);
+
+    close(srv_sock);
+    logger_notice_f(server.log, "Exiting with code '%d'!", exit_code);
+  }
+
+  exit(exit_code);
 }
 
